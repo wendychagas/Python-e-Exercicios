72602c22ddc3969d89d62a6043723d88e73e3e56
wendychagas <git.wendychagas@gmail.com>
2022-09-28 20:49:34 -0300

    Commit and Push.

---
+++ CursoPython/PrimeirosComandos.py
@@ -0,0 +1,10 @@
+nome = 'Wendy'
+idade = 27
+peso = 59.9
+print(nome, idade, peso)
+
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Qual é o seu peso? ')
+print(nome, idade, peso)
+
