bb465403862583bd111b1750059b8beda506a81c
wendychagas <git.wendychagas@gmail.com>
2022-10-02 23:02:10 -0300

    Simplificando e Pulando Linha

--- Mundo 01 - Fundamentos/Exercicios/ex006.py
+++ Mundo 01 - Fundamentos/Exercicios/ex006.py
@@ -5,3 +5,11 @@ triplo =  n1 * 3
 raizQ = n1 **(1/2)
 print(f'Seu número = {n1} \n O dobro = {dobro} \n O triplo = {triplo} \n A raiz quadrada = {raizQ:.2f}')
 #-------------------------------------------------------------
+print('\n')
+#-------------------------------------------------------------
+n2 = int(input('Digite outro número: '))
+print(f'Seu número = {n2}\n Dobro = {n2*2}\n Triplo = {n2*3}\n raizQ = {n2**(1/2):.2f}')
+
+
+
+
