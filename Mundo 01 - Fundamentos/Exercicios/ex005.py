978d86a6de242f5f934a701a04e60a220f9f453b
wendychagas <git.wendychagas@gmail.com>
2022-10-02 22:50:07 -0300

    Descobri como fazer tudo em uma linha apenas.

--- Mundo 01 - Fundamentos/Exercicios/ex005.py
+++ Mundo 01 - Fundamentos/Exercicios/ex005.py
@@ -4,3 +4,6 @@ sucessor = n1+1
 antecessor = n1-1
 print(f'Seu número = {n1} \n O antecessor = {antecessor} \n O sucessor = {sucessor}')
 #-------------------------------------------------------------
+n2 = int(input('Digite outro número: '))
+print(f'Seu número = {n2} \nAntecessor = {n2-1} \nSucessor = {n2+1}')
+
