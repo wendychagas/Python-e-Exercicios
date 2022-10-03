288f0dc1daada7cf67191deb31b87c2a28040df9
wendychagas <git.wendychagas@gmail.com>
2022-10-02 23:20:06 -0300

    Simplificando e Pulando Linha

--- Mundo 01 - Fundamentos/Exercicios/ex007.py
+++ Mundo 01 - Fundamentos/Exercicios/ex007.py
@@ -1,6 +1,16 @@
 #-------------------------------------------------------------
 n1 = int(input('Nota 1: '))
 n2 = int(input('Nota 2: '))
-m = (n1+n2) / 2
+n3 = int(input('Nota 3: '))
+n4 = int(input('Nota 4: '))
+m = (n1+n2+n3+n4) / 4
 print(f'A média = {m:.2f}') # formatado com duas casas decimais;
 #-------------------------------------------------------------
+print('\n')
+#-------------------------------------------------------------
+v1 = int(input('Prova 1: '))
+v2 = int(input('Prova 2: '))
+print(f'A média = {(v1+v2)/2:.2f}')
+
+
+
