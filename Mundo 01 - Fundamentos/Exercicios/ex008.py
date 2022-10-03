0c471722ccc6f3f194aa359d5539a4583e1a8548
wendychagas <git.wendychagas@gmail.com>
2022-10-03 00:26:08 -0300

    Ex008, Ex009

---
+++ Mundo 01 - Fundamentos/Exercicios/ex008.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------
+m = float(input('Medida (M) = '))
+dm = m*10 # decimetro x10
+cm = m*100 # centimetro x100
+mm = m*1000 # milimetro x1000
+dam = m/10 # decâmetro / 10
+hec = m/100 # hectometro / 100
+km = m/1000 # kilometro / 1000
+print(f'Metros = {m}')
+print(f'Centimetros = {cm}')
+print(f'Milímetros = {mm}')
+print(f'Decâmetro = {dam}')
+print(f'Hectometro = {hec}')
+print(f'Kilometro = {km}')
+#-------------------------------------------------------------
+
+
+
