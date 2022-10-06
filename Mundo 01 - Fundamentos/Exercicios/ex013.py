79e919dd3298c51d9365829ac26365c100e1f6a4
wendychagas <git.wendychagas@gmail.com>
2022-10-05 23:51:02 -0300

    Usando outra sintaxe

--- Mundo 01 - Fundamentos/Exercicios/ex013.py
+++ Mundo 01 - Fundamentos/Exercicios/ex013.py
@@ -3,3 +3,10 @@ taxaAumento = float(input('Informe a taxa de aumento %'))
 valorAumento = salarioAtual * taxaAumento / 100
 print(f'Seu novo salário com {taxaAumento:.2f}% de aumento é de R${salarioAtual+valorAumento:.2f}')
 print(f'Recebeu um aumento de R${valorAumento:.2f}')
+
+# Outra forma de resolver:
+
+salario = float(input('Salário atual R$'))
+desconto = float(input('Aumento de %'))
+novoSalario = salario + (salario * desconto / 100)
+print(f'Meu novo salário é de R${novoSalario:.2f}')
