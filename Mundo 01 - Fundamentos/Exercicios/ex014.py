6ddc1c2a334c743ac6d0ded0a21e06f0ca1ca6c6
wendychagas <git.wendychagas@gmail.com>
2022-10-06 00:54:03 -0300

    Espaço entre os operandos para melhor leitura.

--- Mundo 01 - Fundamentos/Exercicios/ex014.py
+++ Mundo 01 - Fundamentos/Exercicios/ex014.py
@@ -1,18 +1,3 @@
-kmPercorridos = float(input('Quantos Km foram percorridos: '))
-diasPassados = float(input('Por quantos dias: '))
-valorKm = 0.15
-valorAluguel = 60
-totPagar = (kmPercorridos*valorKm) + (valorAluguel * diasPassados)
-print(f'Total a pagar é R${totPagar:.2f}')
-
-# método por entrada do usuário
-
-precoKilometro = float(input('Preço do Km R$'))
-kmAndados = float(input('Quantos Km foram percorridos: '))
-precoAluguel = float(input('Preço da diária R$'))
-totalDias = float(input('Quantos dias: '))
-print(f'Total à pagar R${(precoKilometro*kmAndados)+(precoAluguel*totalDias):.2f}')
-
-
-
-
+celsius = float(input('Informe a temperatura ºC: '))
+fahreneit = ((9 * celsius) / 5) + 32
+print(f'{celsius:.1f}ºC = {fahreneit:.1f}ºF')
