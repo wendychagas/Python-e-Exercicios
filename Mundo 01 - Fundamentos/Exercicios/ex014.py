e9070f677075fb326981cee38c12596aed8cfbf7
wendychagas <git.wendychagas@gmail.com>
2022-10-06 00:36:18 -0300

    Com/sem entrada do usuário

---
+++ Mundo 01 - Fundamentos/Exercicios/ex014.py
@@ -0,0 +1,18 @@
+kmPercorridos = float(input('Quantos Km foram percorridos: '))
+diasPassados = float(input('Por quantos dias: '))
+valorKm = 0.15
+valorAluguel = 60
+totPagar = (kmPercorridos*valorKm) + (valorAluguel * diasPassados)
+print(f'Total a pagar é R${totPagar:.2f}')
+
+# método por entrada do usuário
+
+precoKilometro = float(input('Preço do Km R$'))
+kmAndados = float(input('Quantos Km foram percorridos: '))
+precoAluguel = float(input('Preço da diária R$'))
+totalDias = float(input('Quantos dias: '))
+print(f'Total à pagar R${(precoKilometro*kmAndados)+(precoAluguel*totalDias):.2f}')
+
+
+
+
